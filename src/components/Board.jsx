741d272a7c0b0c228cdf197aa549757231c5aaa1
Renda02 <rendaniluvhengo89@gmail.com>
2021-02-22 14:03:37 +0100

    Added more functionality

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import Color from "color";
 
@@ -6,12 +6,12 @@ const colorList = [
   "#ffda79",
   "#e74c3c",
   "#1abc9c",
-  "#84817a",
-  "#2ecc71",
+  "#2c3e50",
   "#ccae62",
+  "#2ecc71",
+  "#84817a",
 
   "#f7f1e3",
-  "#2c3e50",
   "#9b59b6",
   "#5e3458",
   "#474787",
@@ -24,16 +24,28 @@ function getRandomArbitrary(min, max) {
 function Board() {
   const [size, setSize] = useState(2);
   const [score, setScore] = useState(0);
-  const [stop, setStop] = useState(0);
-  const [counter, setCounter] = React.useState(10);
-  
+  const [players, setPlayers] = useState([]);
+  const [difficulty, setDifficulty] = useState(1);
+
+  // counter try
+  const [timerId, setTimerId] = useState([]);
+  const [counter, setCounter] = useState(10);
 
   const cubes = [];
 
-  //time
+  // counter trial
+  /*
   useEffect(() => {
-    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    if (counter > 0) {
+      const id = setTimeout(() => {
+        setCounter(counter - 1);
+      }, 1000);
+      setTimerId(id);
+    } else if (counter === 0) {
+      stopGame();
+    }
   }, [counter]);
+  */
 
   // chose a random cube
   const randomCubeIndex = getRandomArbitrary(0, size * size - 1);
@@ -45,34 +57,35 @@ function Board() {
 
   //stop game
   const stopGame = () => {
-   
-    setStop();
-    
+    setPlayers([...players, score]);
     setScore(0);
     setSize(2);
   };
 
   // go to next step
   const onSpecialCubeClick = () => {
-    // if (score === 4) {
-    //let name = prompt("What is your name?");
-    //alert(`Congratulations , ${name} . You've completed the game!! `);
-    //alert(`Your score is ${score}.`)
-    //} else {
+    // stops timer
+    clearTimeout(timerId);
     setSize(size + 1);
     setScore(score + 1);
-   setCounter(15);
+    setCounter(10);
   };
+
+  //difficulty
+  let saturation = 0.6;
+  if (difficulty === 2) {
+    saturation = 0.7;
+  } else if (difficulty === 3) {
+    saturation = 0.9;
+  }
 
   for (let i = 0; i < size * size; i++) {
     if (i === randomCubeIndex) {
       // special cube
       cubes.push({
-        color: Color(cubeColor).saturate(0.6),
+        color: Color(cubeColor).saturate(saturation),
         onclick: onSpecialCubeClick,
       });
-      
-      
     } else {
       cubes.push({
         color: cubeColor,
@@ -81,6 +94,11 @@ function Board() {
     }
   }
 
+  const restartWithDiff = (diff) => {
+    setDifficulty(diff);
+    stopGame();
+  };
+
   //cube sizing
   const width = `calc(${(1 / size) * 100}% - ${2 * size}px)`;
   const height = `calc(${(1 / size) * 100}% - ${2 * size}px`;
@@ -88,17 +106,26 @@ function Board() {
   return (
     <>
       <div>
-<h2>Time Remaining:{counter}</h2>
+        <h2>Difficulty</h2>
+        <div>
+          <button onClick={() => restartWithDiff(2)}>2</button>
+          <button onClick={() => restartWithDiff(3)}>3</button>
+        </div>
+        <h2>Time Remaining:{counter}</h2>
         <h3>Scoreboard: {score}</h3>
-
         <div className="board">
           {cubes.map((cb) => (
             <div
               className="cube"
               style={{ width, height, backgroundColor: cb.color }}
               onClick={cb.onclick}
-             
             ></div>
+          ))}
+        </div>
+        <div>
+          <h3>Leaderboard:</h3>
+          {players.map((score, index) => (
+            <p>{`Player ${index} : ${score}`}</p>
           ))}
         </div>
       </div>
